0a0d3cd9bf3994fc0b4c1647a51d909815ef4f1f
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2023-06-16 10:19:09 -0300

    feat: add store data api

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,21 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    quotes: null,
+    urlApi: "https://quote-garden.onrender.com/api/v3/quotes",
+  },
+  mutations: {
+    SET_QUOTES(state, payload) {
+      state.quotes = payload;
+    },
+  },
+  actions: {
+    async setQuotes(context, payload = "") {
+      const response = await fetch(`${context.state.urlApi}${payload}`);
+      const data = await response.json();
+
+      context.commit("SET_QUOTES", data);
+    },
+  },
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="home">Hello World!</div>
+  <div class="home">Hello World!{{ quotes }}</div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "HomeView",
+  computed: {
+    ...mapState(["quotes"]),
+  },
+  created() {
+    this.$store.dispatch("setQuotes");
+  },
 };
 </script>
