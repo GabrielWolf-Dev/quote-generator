52bacf796c351e2f14a59f8e67bd4a366b5165f7
Gabriel Wolf <65361452+GabrielWolf-Dev@users.noreply.github.com>
2023-06-16 12:47:34 -0300

    feat: add font and move assets folder to public

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,24 @@
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+
   --color-white: #ffffff;
   --color-black: #000000;
   --color-gray-01: #333333;
   --color-gray-02: #4f4f4f;
   --color-gray-03: #828282;
   --color-yellow: #f7df94;
+
+  font-family: "Raleway", sans-serif;
+  --fontw-default: 500;
+  --fontw-bold: 700;
+  --font-14: 0.875rem;
+  --font-18: 1.125rem;
+  --font-24: 1.5rem;
+  --font-36: 2.25rem;
+}
+
+:root {
+  font-size: 16px;
 }
 </style>
